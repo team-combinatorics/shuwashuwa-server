5f888c7cc10f3ad2bc2a1d7d08cc92b756a9b840
kinami <sunhaonan331@outlook.com>
2020-11-22 01:44:05 +0800

    项目初始化

---
+++ src/main/java/team/combinatorics/shuwashuwa/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package team.combinatorics.shuwashuwa.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team.combinatorics.shuwashuwa.service.HelloWorldService;
+
+@RestController
+@AllArgsConstructor
+public class HelloWorldController {
+
+    private final HelloWorldService helloWorldService;
+
+    @GetMapping("/test/hello")
+    public String helloWorld() {
+        return helloWorldService.sayHello();
+    }
+
+}
---
+++ src/test/java/team/combinatorics/shuwashuwa/ShuwashuwaApplicationTests.java
@@ -0,0 +1,13 @@
+package team.combinatorics.shuwashuwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShuwashuwaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
