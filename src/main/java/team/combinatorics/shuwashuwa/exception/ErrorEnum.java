2c0d38e4e729acbb244576161fc77d80b9fc65f1
Leo_h <libaolinsy@163.com>
2020-11-27 20:17:51 +0800

    修正了error码冲突的问题 将ErrorEnum的属性设置为final

--- src/main/java/team/combinatorics/shuwashuwa/exception/ErrorEnum.java
+++ src/main/java/team/combinatorics/shuwashuwa/exception/ErrorEnum.java
@@ -8,13 +8,13 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ErrorEnum {
-    TOKEN_LOST(40001,"当前操作需要token"),
-    AUTHORITY_UNMATCHED(40002,"当前账号不具有执行指定操作的权限"),
-    WECHAT_SERVER_CONNECTION_FAILURE(40003,"微信服务器连接错误"),
-    CODE2SESSION_FAILURE(40004,"用户身份解析错误"),
-    TOKEN_EXPIRED(40005,"token过期"),
-    TOKEN_INVALID(40006,"token无效");
+    TOKEN_LOST(40002,"当前操作需要token"),
+    AUTHORITY_UNMATCHED(40003,"当前账号不具有执行指定操作的权限"),
+    WECHAT_SERVER_CONNECTION_FAILURE(40004,"微信服务器连接错误"),
+    CODE2SESSION_FAILURE(40005,"用户身份解析错误"),
+    TOKEN_EXPIRED(40006,"token过期"),
+    TOKEN_INVALID(40007,"token无效");
 
-    private Integer errCode;
-    private String errMsg;
+    private final Integer errCode;
+    private final String errMsg;
 }
