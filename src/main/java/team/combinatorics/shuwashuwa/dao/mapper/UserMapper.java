81d9cc643e2210d865767b975ef1a31bd79cf85c
kinami <sunhaonan331@outlook.com>
2020-11-28 21:09:23 +0800

    UserMapper接口

---
+++ src/main/java/team/combinatorics/shuwashuwa/dao/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package team.combinatorics.shuwashuwa.dao.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
+
+@Mapper
+@Component(value = "userMapper")
+public interface UserMapper {
+}
