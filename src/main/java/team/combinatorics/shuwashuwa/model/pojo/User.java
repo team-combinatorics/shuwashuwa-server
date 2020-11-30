82b01729464e919dd44b3f02dd99bb3c05755788
kinami <sunhaonan331@outlook.com>
2020-12-01 03:40:40 +0800

    将原来蹩脚的下划线命名改成了更java的驼峰命名法

--- src/main/java/team/combinatorics/shuwashuwa/model/pojo/User.java
+++ src/main/java/team/combinatorics/shuwashuwa/model/pojo/User.java
@@ -1,26 +1,28 @@
 package team.combinatorics.shuwashuwa.model.pojo;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
-    private int userid;
+    private Integer userid;
     private String openid;
-    private String user_name;
-    private String nick_name;
-    private String phone_number;
+    private String userName;
+    private String nickName;
+    private String phoneNumber;
     private String email;
     private String identity;
     private String department;
     private String grade;
-    private String student_id;
+    private String studentId;
     private String comment;
-    private boolean is_volunteer;
-    private boolean is_admin;
-    private boolean is_su;
+    private boolean isVolunteer;
+    private boolean isAdmin;
+    private boolean isSu;
 }
