a146690217184af7e26362afbf6b18ebf4e25d74
kinami <sunhaonan331@outlook.com>
2020-12-01 05:44:43 +0800

    添加了一个用于添加一些虚假用户信息的测试用接口，完善了api说明

--- src/main/java/team/combinatorics/shuwashuwa/controller/TestController.java
+++ src/main/java/team/combinatorics/shuwashuwa/controller/TestController.java
@@ -1,6 +1,8 @@
 package team.combinatorics.shuwashuwa.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,7 @@ import team.combinatorics.shuwashuwa.annotation.AdminAccess;
 import team.combinatorics.shuwashuwa.annotation.AllAccess;
 import team.combinatorics.shuwashuwa.annotation.NoToken;
 import team.combinatorics.shuwashuwa.annotation.VolunteerAccess;
+import team.combinatorics.shuwashuwa.dao.UserDao;
 
 @Api(value = "测试用接口")
 @RestController
@@ -23,6 +26,10 @@ public class TestController {
     @Value("${spring.datasource.dbcp2.password}")
     String mysqlPassword;
 
+    @Autowired
+    UserDao userDao;
+
+    @ApiOperation("获取当前数据库地址、用户名、密码，用于查看环境变量是否注入成功")
     @GetMapping("/env")
     public String testEnv() {
         return "url = " + mysqlUrl + "\n"
@@ -30,27 +37,42 @@ public class TestController {
                 + "password = " + mysqlPassword + "\n";
     }
 
+    @ApiOperation("hello,world")
     @GetMapping("/auth")
     @NoToken
     public String ordinaryHelloworld() {
         return "Hello, world";
     }
 
+    @ApiOperation("测试身份：所有用户")
     @GetMapping("/auth/default")
     @AllAccess
     public String clientHelloworld() {
         return "Welcome, client!";
     }
 
+    @ApiOperation("测试身份：志愿者")
     @GetMapping("/auth/volunteer")
     @VolunteerAccess
     public String volunteerHelloworld() {
         return "Welcome, volunteer!";
     }
 
+    @ApiOperation("测试身份：管理员")
     @GetMapping("/auth/admin")
     @AdminAccess
     public String adminHelloworld() {
         return "Welcome, admin!";
+    }
+
+    @ApiOperation("添加4个假身份，测试用")
+    @GetMapping("/addUser")
+    @NoToken
+    public String addFakeUsers() {
+        userDao.addUserOpenid("fake openid 1");
+        userDao.addUserOpenid("fake openid 2");
+        userDao.addUserOpenid("fake openid 3");
+        userDao.addUserOpenid("fake openid 4");
+        return "dokidoki";
     }
 }
