85e3c77f3c780ad4cc7e8c86945bec102887f5d3
kinami <sunhaonan331@outlook.com>
2020-12-01 03:36:46 +0800

    删除了暂时无用的UserMapper

--- src/main/java/team/combinatorics/shuwashuwa/dao/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package team.combinatorics.shuwashuwa.dao.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Component;
-
-@Mapper
-@Component(value = "userMapper")
-public interface UserMapper {
-}
