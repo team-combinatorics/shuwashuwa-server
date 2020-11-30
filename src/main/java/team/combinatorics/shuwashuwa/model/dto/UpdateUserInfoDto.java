6c0a461b0f978da2c3cdaaf7e23cd430d5e6c5f5
kinami <sunhaonan331@outlook.com>
2020-12-01 05:44:00 +0800

    为更新用户信息时的数据传输对象添加了api注解

--- src/main/java/team/combinatorics/shuwashuwa/model/dto/UpdateUserInfoDto.java
+++ src/main/java/team/combinatorics/shuwashuwa/model/dto/UpdateUserInfoDto.java
@@ -1,6 +1,9 @@
 package team.combinatorics.shuwashuwa.model.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,16 +12,27 @@ import lombok.NoArgsConstructor;
  * @version 0.0.1
  */
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel("更新用户信息时传输的数据")
 public class UpdateUserInfoDto {
-    private String user_name;
-    private String nick_name;
-    private String phone_number;
+    @ApiModelProperty("用户姓名")
+    private String userName;
+    @ApiModelProperty("用户昵称")
+    private String nickName;
+    @ApiModelProperty("用户手机号")
+    private String phoneNumber;
+    @ApiModelProperty("用户邮箱")
     private String email;
+    @ApiModelProperty("用户身份")
     private String identity;
+    @ApiModelProperty("用户所在部门")
     private String department;
+    @ApiModelProperty("年级")
     private String grade;
-    private String student_id;
+    @ApiModelProperty("学号")
+    private String studentId;
+    @ApiModelProperty("备注")
     private String comment;
 }
