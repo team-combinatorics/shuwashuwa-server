bfc26efd31cb7cd62f049f832145ede2b43a6c2e
kinami <sunhaonan331@outlook.com>
2020-12-02 02:13:12 +0800

    修复了把申请表写成维修单的bug

--- src/main/java/team/combinatorics/shuwashuwa/dao/VolunteerApplicationDao.java
+++ src/main/java/team/combinatorics/shuwashuwa/dao/VolunteerApplicationDao.java
@@ -17,16 +17,18 @@ public interface VolunteerApplicationDao {
     void insert(@Param("id") int id, @Param("application") VolunteerApplicationDTO volunteerApplicationDTO);
 
     /**
-     * 通过用户id寻找维修单，可能有多个结果，因此返回一个列表
+     * 通过用户id寻找申请表，可能有多个结果，因此返回一个列表
      * @param id 用户id
-     * @return 维修单列表
+     * @return 申请表列表
      */
     List<VolunteerApplicationDO> selectByUserId(@Param("id") int id);
 
     /**
-     * 通过维修单id寻找维修单，只有一个结果
-     * @param id 维修单id
-     * @return 维修单对象
+     * 通过申请表id寻找申请表，只有一个结果
+     * @param id 申请表id
+     * @return 申请表对象
      */
     VolunteerApplicationDO selectByFormId(@Param("id") int id);
+
+
 }
