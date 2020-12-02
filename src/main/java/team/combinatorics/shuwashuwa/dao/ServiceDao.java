b3444164eb185a06a0b9a83db1b95fba6a997658
kinami <sunhaonan331@outlook.com>
2020-12-03 04:15:42 +0800

    文件删除：删除了无用的ServiceDao

--- src/main/java/team/combinatorics/shuwashuwa/dao/ServiceDao.java
+++
@@ -1,9 +0,0 @@
-package team.combinatorics.shuwashuwa.dao;
-
-import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface ServiceDao {
-
-}
