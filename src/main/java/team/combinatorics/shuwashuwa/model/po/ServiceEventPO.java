affa68dfc799d1c75abe8a7514ec84a92569a6db
kinami <sunhaonan331@outlook.com>
2020-12-03 00:55:35 +0800

    添加了和维修单相关的三个PO

---
+++ src/main/java/team/combinatorics/shuwashuwa/model/po/ServiceEventPO.java
@@ -0,0 +1,23 @@
+package team.combinatorics.shuwashuwa.model.po;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServiceEventPO {
+    private Integer id;
+    private Timestamp createTime;
+    private Timestamp updatedTime;
+    private String repairingResult;
+    private Integer status;
+    private String feedback;
+    private Integer activityId;
+    private Integer timeSlot;
+}
--- src/main/java/team/combinatorics/shuwashuwa/model/po/ServiceFormPO.java
+++ src/main/java/team/combinatorics/shuwashuwa/model/po/ServiceFormPO.java
@@ -1,4 +1,4 @@
-package team.combinatorics.shuwashuwa.model.pojo;
+package team.combinatorics.shuwashuwa.model.po;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -8,12 +8,11 @@ import lombok.NoArgsConstructor;
 import java.sql.Date;
 import java.sql.Timestamp;
 
-
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class ServiceFormDO {
+public class ServiceFormPO {
     private Integer id;
     private Timestamp createTime;
     private Timestamp updatedTime;
@@ -27,10 +26,6 @@ public class ServiceFormDO {
     private boolean underWarranty;
     private String problemDescription;
     private String problemType;
-    private String decriptionEditingAdvice;
-    private String repairingResult;
+    private String decriptionAdvice;
     private Integer status;
-    private String feedback;
-    private Integer activityId;
-    private Integer timeSlot;
 }
