6cac1218028c1f80661647f615210d381568c1ef
Leo_h <libaolinsy@163.com>
2020-12-03 13:38:01 +0800

    声明了处理志愿者申请所需的接口

--- src/main/java/team/combinatorics/shuwashuwa/service/UserService.java
+++ src/main/java/team/combinatorics/shuwashuwa/service/UserService.java
@@ -1,10 +1,11 @@
 package team.combinatorics.shuwashuwa.service;
 
 import org.springframework.stereotype.Service;
-import team.combinatorics.shuwashuwa.model.dto.LogInInfoDTO;
-import team.combinatorics.shuwashuwa.model.dto.LogInSuccessDTO;
-import team.combinatorics.shuwashuwa.model.dto.UpdateUserInfoDTO;
+import team.combinatorics.shuwashuwa.model.dto.*;
 import team.combinatorics.shuwashuwa.model.po.UserPO;
+import team.combinatorics.shuwashuwa.model.po.VolunteerApplicationPO;
+
+import java.util.List;
 
 @Service
 public interface UserService {
@@ -18,6 +19,12 @@ public interface UserService {
     void updateUserInfo(int userid, UpdateUserInfoDTO updateUserInfoDto) throws Exception;
 
     UserPO getUserInfo(int userid) throws Exception;
+
+    void addVolunteerApplication(int userid, VolunteerApplicationDTO volunteerApplicationDTO);
+
+    List<VolunteerApplicationPO> getUnauditedVolunteerApplicationList();
+
+    void updateVolunteerApplication(int formid, int userid, VolunteerApplicationUpdateDTO updateDTO);
 
     // boolean suicide();
 
