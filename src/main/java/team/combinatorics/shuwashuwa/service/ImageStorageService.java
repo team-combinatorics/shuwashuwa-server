7fd6b3579e7d0afb53b78dd8cad9c6cf25020d2d
Leo_h <libaolinsy@163.com>
2020-12-04 01:19:03 +0800

    定义并实现了保存删除图片的service，虽然带了三个warning

---
+++ src/main/java/team/combinatorics/shuwashuwa/service/ImageStorageService.java
@@ -0,0 +1,28 @@
+package team.combinatorics.shuwashuwa.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageStorageService {
+
+    /**
+     * 存储一个文件
+     * @param userid 发起图片上传的用户id
+     * @param file 要存储的文件
+     * @return 文件被存储到的路径
+     */
+    String store(int userid, MultipartFile file);
+
+    /**
+     * 删除一个文件
+     * @param userid 发起图片删除的用户id
+     * @param path 待删除的文件路径
+     */
+    void remove(int userid, String path);
+
+    /**
+     * 确认一个文件与维修单关联，从而去除缓存属性
+     * @param userid 释放缓存空间的用户id
+     * @param path 待处理的文件路径
+     */
+    void confirm(int userid, String path);
+}
---
+++ src/main/java/team/combinatorics/shuwashuwa/service/impl/ImageStorageServiceImpl.java
@@ -0,0 +1,72 @@
+package team.combinatorics.shuwashuwa.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import team.combinatorics.shuwashuwa.exception.ErrorInfoEnum;
+import team.combinatorics.shuwashuwa.exception.KnownException;
+import team.combinatorics.shuwashuwa.service.ImageStorageService;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+@RequiredArgsConstructor
+@Service
+public class ImageStorageServiceImpl implements ImageStorageService {
+
+    // TODO: 从环境中读取文件目录（需要目录存在
+    private static final Path storageDir = Paths.get("D:\\");
+
+    private static final Map<Integer, List<Path>> unconfirmed = new HashMap<>();
+
+    private static final int SINGLE_USER_QUEUE_LIMIT = 9;
+
+    @Override
+    public String store(int userid, MultipartFile file) throws KnownException {
+        //生成随机唯一的文件名，但保留后缀
+        String receivedFileName = file.getOriginalFilename();
+        String fileType = receivedFileName.substring(receivedFileName.lastIndexOf("."));
+        String fileName = UUID.randomUUID().toString()+fileType;
+        Path path = storageDir.resolve(fileName);
+
+        //尝试存储
+        try {
+            file.transferTo(path);
+        } catch (IOException ioe) {
+            throw new KnownException(ErrorInfoEnum.STORAGE_FAILURE);
+        }
+
+        //检查缓存图片队列
+        if (unconfirmed.containsKey(userid)) {
+            if (unconfirmed.get(userid).size() == SINGLE_USER_QUEUE_LIMIT) {
+                unconfirmed.get(userid).remove(0).toFile().delete();
+            }
+        }
+        else {
+            unconfirmed.put(userid,new Vector<>());
+        }
+
+        //插入缓存图片队列
+        unconfirmed.get(userid).add(path);
+
+        return path.toString();
+    }
+
+    @Override
+    public void remove(int userid, String path) {
+        //删除当然是"确认"图片没有用
+        confirm(userid, path);
+
+        Path.of(path).toFile().delete();
+    }
+
+    @Override
+    public void confirm(int userid, String path) {
+        if(!unconfirmed.get(userid).remove(Path.of(path)))
+            throw new KnownException(ErrorInfoEnum.IMAGE_NOT_CACHED);
+        if(unconfirmed.get(userid).size()==0)
+            unconfirmed.get(userid).remove(userid);
+    }
+}
