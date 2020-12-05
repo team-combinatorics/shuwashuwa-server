ad0caa348a034d86039ce94d7a46b6ba6d2b19c5
kinami <sunhaonan331@outlook.com>
2020-12-06 01:51:39 +0800

    Test更新：调价了一个使用md5工具的示例

--- src/test/java/team/combinatorics/shuwashuwa/ShuwashuwaApplicationTests.java
+++ src/test/java/team/combinatorics/shuwashuwa/ShuwashuwaApplicationTests.java
@@ -2,12 +2,16 @@ package team.combinatorics.shuwashuwa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import team.combinatorics.shuwashuwa.utils.MD5Util;
 
 @SpringBootTest
 class ShuwashuwaApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println(MD5Util.getMD5("Tsugudaisuki"));
+        System.out.println(MD5Util.getMD5("mskcrawl"));
+
     }
 
 }
