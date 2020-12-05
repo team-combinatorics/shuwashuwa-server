1716659d781472bec5c22ebc9033df12e86676fb
kinami <sunhaonan331@outlook.com>
2020-12-06 03:06:24 +0800

    DAO方法更新：实现了对管理员操作的一系列接口

--- src/main/java/team/combinatorics/shuwashuwa/dao/AdminDao.java
+++ src/main/java/team/combinatorics/shuwashuwa/dao/AdminDao.java
@@ -17,7 +17,7 @@ public interface AdminDao {
     int insert(@Param("admin") AdminPO adminPO);
 
     /**
-     * 修改管理员信息
+     * 修改管理员信息，不允许修改userid，不能全为空
      *
      * @param adminPO 管理员信息
      * @return 修改成功的记录数量
@@ -40,11 +40,19 @@ public interface AdminDao {
     AdminPO getByID(@Param("id") int id);
 
     /**
+     * 根据用户id获取管理员id
+     * @param userID 用户id
+     * @return 管理员id
+     */
+    int getAdminIDByUserID(@Param("userID") int userID);
+
+    /**
      * 根据id删除管理员
+     *
      * @param id 管理员id
      * @return 删除的数量，应该为1
      */
-    int deleteByID(@Param("id")int id);
+    int deleteByID(@Param("id") int id);
 
 
 }
