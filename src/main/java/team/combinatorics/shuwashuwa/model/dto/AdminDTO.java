8b5c76951591b5f1ec6b8f949f63b89267237efa
leesou <1181902665@qq.com>
2020-12-07 00:17:10 +0800

    添加了用于传输管理员信息的DTO

---
+++ src/main/java/team/combinatorics/shuwashuwa/model/dto/AdminDTO.java
@@ -0,0 +1,36 @@
+package team.combinatorics.shuwashuwa.model.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ApiModel("添加或修改管理员信息时要上传的数据")
+public class AdminDTO {
+    @ApiModelProperty("用户id，用于标识用户")
+    private String userid;
+
+    @ApiModelProperty("管理员（真实）姓名")
+    private String userName;
+
+    @ApiModelProperty("管理员手机号")
+    private String phoneNumber;
+
+    @ApiModelProperty("管理员邮箱地址")
+    private String email;
+
+    @ApiModelProperty("管理员身份")
+    private String identity;
+
+    @ApiModelProperty("管理员所在学部")
+    private String department;
+
+    @ApiModelProperty("管理员学号")
+    private String studentId;
+}
