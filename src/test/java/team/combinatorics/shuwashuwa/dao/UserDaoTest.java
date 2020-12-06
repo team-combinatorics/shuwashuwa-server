e8ad0ff4e0beba26006c9dbf3ad5b4db6ef1b06d
leesou <1181902665@qq.com>
2020-12-06 18:43:08 +0800

    把UserDaoTest中原有的打印测试修改为Assert语句的测试

--- src/test/java/team/combinatorics/shuwashuwa/dao/UserDaoTest.java
+++ src/test/java/team/combinatorics/shuwashuwa/dao/UserDaoTest.java
@@ -18,29 +18,59 @@ public class UserDaoTest {
 
     @Test
     public void weakTest() {
-        userDao.insertUserByOpenid("fake openid 1");
-        userDao.insertUserByOpenid("fake openid 2");
-        userDao.insertUserByOpenid("fake openid 3");
-        int ans = userDao.insertUserByOpenid("fake openid 4");
-        Assert.assertEquals(1, ans);
-        // 更改一个用户信息测试
+        // 插入单个用户的测试
+        Assert.assertEquals(1, userDao.insertUserByOpenid("fake openid 1"));
+        Assert.assertEquals(1, userDao.insertUserByOpenid("fake openid 2"));
+        Assert.assertEquals(1, userDao.insertUserByOpenid("fake openid 3"));
+        Assert.assertEquals(1, userDao.insertUserByOpenid("fake openid 4"));
+        Assert.assertEquals(1, userDao.insertUserByOpenid("fake openid 5"));
+        Assert.assertEquals(1, userDao.insertUserByOpenid("fake openid 6"));
+
+        // 更改一个用户信息的测试，同时测试利用openid和userid来查找这个被修改的用户，并获取被修改的信息
+        // 更改userName
         userDao.updateUserInfo(1, UpdateUserInfoDTO.builder()
                 .userName("misaki")
                 .build());
+        Assert.assertEquals("misaki", userDao.getUserByUserid(1).getUserName());
+        Assert.assertEquals("misaki", userDao.getUserByOpenid("fake openid 1").getUserName());
 
-        // 根据openid查找测试
-        System.out.println(userDao.getUserByOpenid("fake openid 1"));
-        // 根据userid查找测试
-        System.out.println(userDao.getUserByUserid(1));
-        System.out.println(userDao.deleteUserByOpenid("fake openid 2"));
-        System.out.println(userDao.deleteUserByUserid(3));
-        userDao.insertUserByOpenid("fake openid 5");
-        userDao.insertUserByOpenid("fake openid 6");
-        userDao.updateUserVolunteerAuthority(4, true);
-        userDao.updateUserAdminAuthority(4, true);
-        System.out.println(userDao.getUserByUserid(4));
-        System.out.println(userDao.deleteAllUsers());
+        // 更改comment
+        userDao.updateUserInfo(1, UpdateUserInfoDTO.builder()
+                .comment("Happy! Lucky! Smile! Yeah!")
+                .build());
+        Assert.assertEquals("Happy! Lucky! Smile! Yeah!", userDao.getUserByUserid(1).getComment());
+        Assert.assertEquals("Happy! Lucky! Smile! Yeah!", userDao.getUserByOpenid("fake openid 1").getComment());
 
+        // 同时更改多个属性
+        userDao.updateUserInfo(2, UpdateUserInfoDTO.builder()
+                .email("114514@1919.810")
+                .comment("24 years old, a student.")
+                .build());
+        Assert.assertEquals("114514@1919.810", userDao.getUserByUserid(2).getEmail());
+        Assert.assertEquals("114514@1919.810", userDao.getUserByOpenid("fake openid 2").getEmail());
+        Assert.assertEquals("24 years old, a student.", userDao.getUserByUserid(2).getComment());
+        Assert.assertEquals("24 years old, a student.", userDao.getUserByOpenid("fake openid 2").getComment());
+
+        // 更改用户权限的测试--志愿者
+        userDao.updateUserVolunteerAuthority(6, true);
+        Assert.assertTrue(userDao.getUserByUserid(6).isVolunteer());
+        Assert.assertTrue(userDao.getUserByOpenid("fake opnid 6").isVolunteer());
+
+        // 更改用户权限的测试--管理员
+        userDao.updateUserAdminAuthority(6, true);
+        Assert.assertTrue(userDao.getUserByUserid(6).isAdmin());
+        Assert.assertTrue(userDao.getUserByOpenid("fake opnid 6").isAdmin());
+
+        // 删除单个用户的测试--使用userid
+        Assert.assertEquals(1, userDao.deleteUserByUserid(1)); //删除一个已存在的用户
+        Assert.assertEquals(0, userDao.deleteUserByUserid(1)); //删除一个不存在（已被删除过）的用户
+
+        // 删除单个用户的测试--使用openid
+        Assert.assertEquals(1, userDao.deleteUserByOpenid("fake openid 2"));
+        Assert.assertEquals(0, userDao.deleteUserByOpenid("114514 1919810"));
+
+        //删除所有用户的测试
+        Assert.assertEquals(1, userDao.deleteAllUsers());
     }
 
 
