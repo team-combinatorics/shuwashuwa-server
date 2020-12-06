7bc82b600d8cbaa27be661bb5a727840ef164c74
leesou <1181902665@qq.com>
2020-12-06 19:03:14 +0800

    Test更新：把UserDaoTest中原有的打印测试修改为Assert语句的测试

--- src/test/java/team/combinatorics/shuwashuwa/dao/UserDaoTest.java
+++ src/test/java/team/combinatorics/shuwashuwa/dao/UserDaoTest.java
@@ -19,37 +19,37 @@ public class UserDaoTest {
     @Test
     public void weakTest() {
         // 插入单个用户的测试
-        Assert.assertEquals(1, userDao.insertUserByOpenid("fake openid 1"));
         Assert.assertEquals(1, userDao.insertUserByOpenid("fake openid 2"));
         Assert.assertEquals(1, userDao.insertUserByOpenid("fake openid 3"));
         Assert.assertEquals(1, userDao.insertUserByOpenid("fake openid 4"));
         Assert.assertEquals(1, userDao.insertUserByOpenid("fake openid 5"));
         Assert.assertEquals(1, userDao.insertUserByOpenid("fake openid 6"));
+        Assert.assertEquals(1, userDao.insertUserByOpenid("fake openid 7"));
 
         // 更改一个用户信息的测试，同时测试利用openid和userid来查找这个被修改的用户，并获取被修改的信息
         // 更改userName
-        userDao.updateUserInfo(1, UpdateUserInfoDTO.builder()
+        userDao.updateUserInfo(2, UpdateUserInfoDTO.builder()
                 .userName("misaki")
                 .build());
-        Assert.assertEquals("misaki", userDao.getUserByUserid(1).getUserName());
-        Assert.assertEquals("misaki", userDao.getUserByOpenid("fake openid 1").getUserName());
+        Assert.assertEquals("misaki", userDao.getUserByUserid(2).getUserName());
+        Assert.assertEquals("misaki", userDao.getUserByOpenid("fake openid 2").getUserName());
 
         // 更改comment
-        userDao.updateUserInfo(1, UpdateUserInfoDTO.builder()
+        userDao.updateUserInfo(2, UpdateUserInfoDTO.builder()
                 .comment("Happy! Lucky! Smile! Yeah!")
                 .build());
-        Assert.assertEquals("Happy! Lucky! Smile! Yeah!", userDao.getUserByUserid(1).getComment());
-        Assert.assertEquals("Happy! Lucky! Smile! Yeah!", userDao.getUserByOpenid("fake openid 1").getComment());
+        Assert.assertEquals("Happy! Lucky! Smile! Yeah!", userDao.getUserByUserid(2).getComment());
+        Assert.assertEquals("Happy! Lucky! Smile! Yeah!", userDao.getUserByOpenid("fake openid 2").getComment());
 
         // 同时更改多个属性
-        userDao.updateUserInfo(2, UpdateUserInfoDTO.builder()
+        userDao.updateUserInfo(3, UpdateUserInfoDTO.builder()
                 .email("114514@1919.810")
                 .comment("24 years old, a student.")
                 .build());
-        Assert.assertEquals("114514@1919.810", userDao.getUserByUserid(2).getEmail());
-        Assert.assertEquals("114514@1919.810", userDao.getUserByOpenid("fake openid 2").getEmail());
-        Assert.assertEquals("24 years old, a student.", userDao.getUserByUserid(2).getComment());
-        Assert.assertEquals("24 years old, a student.", userDao.getUserByOpenid("fake openid 2").getComment());
+        Assert.assertEquals("114514@1919.810", userDao.getUserByUserid(3).getEmail());
+        Assert.assertEquals("114514@1919.810", userDao.getUserByOpenid("fake openid 3").getEmail());
+        Assert.assertEquals("24 years old, a student.", userDao.getUserByUserid(3).getComment());
+        Assert.assertEquals("24 years old, a student.", userDao.getUserByOpenid("fake openid 3").getComment());
 
         // 更改用户权限的测试--志愿者
         userDao.updateUserVolunteerAuthority(6, true);
@@ -62,15 +62,22 @@ public class UserDaoTest {
         Assert.assertTrue(userDao.getUserByOpenid("fake opnid 6").isAdmin());
 
         // 删除单个用户的测试--使用userid
-        Assert.assertEquals(1, userDao.deleteUserByUserid(1)); //删除一个已存在的用户
-        Assert.assertEquals(0, userDao.deleteUserByUserid(1)); //删除一个不存在（已被删除过）的用户
+        Assert.assertEquals(1, userDao.deleteUserByUserid(4)); //删除一个已存在的用户
+        Assert.assertEquals(0, userDao.deleteUserByUserid(4)); //删除一个不存在（已被删除过）的用户
 
         // 删除单个用户的测试--使用openid
-        Assert.assertEquals(1, userDao.deleteUserByOpenid("fake openid 2"));
+        Assert.assertEquals(1, userDao.deleteUserByOpenid("fake openid 5"));
         Assert.assertEquals(0, userDao.deleteUserByOpenid("114514 1919810"));
 
-        //删除所有用户的测试
+        // （不知道需不需要，先写上去了）测试超管的用户名密码获取，以及密码修改的正确性
+        Assert.assertEquals("shuwashuwa", userDao.getSuUsername());
+        Assert.assertEquals("1da5505af2a5ba46a749eaa6b1a92003", userDao.getSuEncryptedPSW());
+        userDao.updateSuPSW("shuwashuwadoridorimin!");
+        Assert.assertEquals("shuwashuwadoridorimin!", userDao.getSuEncryptedPSW());
+
+        // 删除所有用户的测试
         Assert.assertEquals(1, userDao.deleteAllUsers());
+
     }
 
 
