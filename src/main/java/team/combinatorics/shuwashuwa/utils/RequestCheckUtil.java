e3e9d1482816e0d780237ea1c6d36289dbfe40d6
Leo_h <libaolinsy@163.com>
2020-12-06 20:58:59 +0800

    增加了一个判断DTO是否有属性为空的工具类

---
+++ src/main/java/team/combinatorics/shuwashuwa/utils/RequestCheckUtil.java
@@ -0,0 +1,38 @@
+package team.combinatorics.shuwashuwa.utils;
+
+import java.lang.reflect.Field;
+
+public class RequestCheckUtil {
+
+    public static boolean fieldAllNull(Object obj) {
+        for (Field field:obj.getClass().getDeclaredFields()){
+            if(field.getName().equals("this$0"))
+                continue;
+            try {
+                field.setAccessible(true);
+                if(field.get(obj) != null)
+                    return false;
+            }
+            catch (IllegalAccessException e){
+                System.out.println(field.getName()+" is not accessible");
+            }
+        }
+        return true;
+    }
+
+    public static boolean fieldExistNull(Object obj) {
+        for (Field field:obj.getClass().getDeclaredFields()){
+            if(field.getName().equals("this$0"))
+                continue;
+            try {
+                field.setAccessible(true);
+                if(field.get(obj) == null)
+                    return true;
+            }
+            catch (IllegalAccessException e){
+                System.out.println(field.getName()+" is not accessible");
+            }
+        }
+        return false;
+    }
+}
