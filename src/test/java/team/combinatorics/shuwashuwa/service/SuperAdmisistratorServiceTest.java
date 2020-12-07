2256821ec66c1ad4b3c7ddfd8e10f056ac005b7b
leesou <1181902665@qq.com>
2020-12-07 21:04:34 +0800

    Test添加：添加了对超管服务层测试的Test类，待完成

---
+++ src/test/java/team/combinatorics/shuwashuwa/service/SuperAdmisistratorServiceTest.java
@@ -0,0 +1,22 @@
+package team.combinatorics.shuwashuwa.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import team.combinatorics.shuwashuwa.MainApplication;
+
+/*TODO: 完成对超管服务层功能的测试*/
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = MainApplication.class)
+public class SuperAdmisistratorServiceTest {
+
+    @Autowired
+    SuperAdministratorService superAdministratorService;
+
+    @Test
+    public void simpleTest() {
+
+    }
+}
