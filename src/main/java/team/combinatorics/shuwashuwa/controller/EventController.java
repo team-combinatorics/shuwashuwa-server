3dfc8529b66982829748a59e091667db329b62ca
leesou <1181902665@qq.com>
2020-12-08 14:41:22 +0800

    维修事件服务添加：初步实现了用户提交维修单的后端逻辑（TODO：补充对维修单草稿状态的处理）

--- src/main/java/team/combinatorics/shuwashuwa/controller/EventController.java
+++ src/main/java/team/combinatorics/shuwashuwa/controller/EventController.java
@@ -5,13 +5,15 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import team.combinatorics.shuwashuwa.annotation.AllAccess;
 import team.combinatorics.shuwashuwa.model.dto.ServiceEventResponseDTO;
+import team.combinatorics.shuwashuwa.model.dto.ServiceFormDTO;
 import team.combinatorics.shuwashuwa.model.pojo.CommonResult;
+import team.combinatorics.shuwashuwa.service.EventService;
+import team.combinatorics.shuwashuwa.utils.TokenUtil;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Api("维修事件接口说明")
@@ -20,11 +22,18 @@ import java.util.List;
 @AllArgsConstructor
 public class EventController {
 
+    private final EventService eventService;
+
     @ApiOperation(value = "处理维修单提交", notes = "若首次提交则创建维修事件，否则归档入指定的", httpMethod = "POST")
     @RequestMapping(value = "", method = RequestMethod.POST)
     @ApiResponses({@ApiResponse(code = 200, message = "请求成功")})
     @AllAccess
-    public CommonResult<String> handleFormCommit() {
+    public CommonResult<String> handleFormCommit(@RequestHeader @NotNull(message = "用户Token不能为空") String token,
+                                                 @RequestBody @NotNull(message = "维修单信息不能为空") ServiceFormDTO serviceFormDTO,
+                                                 @RequestBody Integer timeSlot,
+                                                 @RequestBody  @NotNull(message = "活动id不能为空") Integer activityID) {
+        int userid = TokenUtil.extractUserid(token);
+        eventService.commitForm(userid, serviceFormDTO, timeSlot, activityID);
         return new CommonResult<>(200,"请求成功","success");
     }
 
