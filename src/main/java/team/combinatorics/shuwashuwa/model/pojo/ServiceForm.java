99c4bf1f38cfca0906c9e9f81dfdb3028990de35
Leo_h <libaolinsy@163.com>
2020-12-11 03:26:57 +0800

    写了api说明

--- src/main/java/team/combinatorics/shuwashuwa/model/pojo/ServiceForm.java
+++ src/main/java/team/combinatorics/shuwashuwa/model/pojo/ServiceForm.java
@@ -1,5 +1,7 @@
 package team.combinatorics.shuwashuwa.model.pojo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,19 +18,47 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel("返回给前端的维修单信息")
 public class ServiceForm {
+    @ApiModelProperty(value = "维修单ID")
     private Integer formID;
+
+    @ApiModelProperty("电脑品牌")
     private String brand;
+
+    @ApiModelProperty("电脑型号")
     private String computerModel;
+
+    @ApiModelProperty("cpu型号")
     private String cpuModel;
+
+    @ApiModelProperty("是否拥有独立显卡")
     private Boolean hasDiscreteGraphics;
+
+    @ApiModelProperty("显卡型号")
     private String graphicsModel;
+
+    @ApiModelProperty("笔记本类型")
     private String laptopType;
+
+    @ApiModelProperty("购买时间")
     private Date boughtTime;
-    private Boolean underWarranty;
+
+    @ApiModelProperty("是否在保修期内")
+    private boolean underWarranty;
+
+    @ApiModelProperty("问题描述")
     private String problemDescription;
+
+    @ApiModelProperty("问题种类--硬件or软件")
     private String problemType;
+
+    @ApiModelProperty("关联到此维修单的图片列表")
     private List<ServicePic> pictures;
+
+    @ApiModelProperty("审核维修单的管理员Id")
     private Integer replyUserId;
+
+    @ApiModelProperty("审核意见")
     private String descriptionAdvice;
 }
