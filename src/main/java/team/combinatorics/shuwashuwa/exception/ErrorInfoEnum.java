d6ce2582a58298ee93a39ff910cd1017aefdac3a
Leo_h <libaolinsy@163.com>
2020-12-10 14:52:26 +0800

    拓宽了40009的语义

--- src/main/java/team/combinatorics/shuwashuwa/exception/ErrorInfoEnum.java
+++ src/main/java/team/combinatorics/shuwashuwa/exception/ErrorInfoEnum.java
@@ -14,7 +14,7 @@ public enum ErrorInfoEnum {
     TOKEN_EXPIRED(40006,"token过期"),
     TOKEN_INVALID(40007,"token无效"),
     STORAGE_FAILURE(40008,"文件存储失败"),
-    IMAGE_NOT_YOURS(40009,"试图访问其他用户上传的图片"),
+    DATA_NOT_YOURS(40009,"试图访问其他用户的数据"),
     PARAMETER_LACKING(40010,"请求结构中缺少必需的参数"),
     WRONG_SUPER_ADMINISTRATOR_INFO(40011, "超级管理员用户名/密码错误"),
     DUPLICATED_PROMOTION(40012,"用户已具有希望获得的身份");
