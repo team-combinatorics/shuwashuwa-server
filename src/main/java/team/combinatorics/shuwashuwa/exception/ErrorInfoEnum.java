83f6dee9a189e338ab54422c181db93f4d96810c
leesou <1181902665@qq.com>
2020-12-14 14:07:47 +0800

    异常类型添加：为获取acces token添加了一个枚举项

--- src/main/java/team/combinatorics/shuwashuwa/exception/ErrorInfoEnum.java
+++ src/main/java/team/combinatorics/shuwashuwa/exception/ErrorInfoEnum.java
@@ -18,7 +18,8 @@ public enum ErrorInfoEnum {
     PARAMETER_LACKING(40010,"请求结构中缺少必需的参数"),
     WRONG_SUPER_ADMINISTRATOR_INFO(40011, "超级管理员用户名/密码错误"),
     DUPLICATED_PROMOTION(40012,"用户已具有希望获得的身份"),
-    STATUS_UNMATCHED(40013,"当前维修状态下不允许执行指定操作");
+    STATUS_UNMATCHED(40013,"当前维修状态下不允许执行指定操作"),
+    ACCSEETOKEN_FAILURE(40014, "获取微信Accsee token 出错");
 
     private final Integer errCode;
     private final String errMsg;
