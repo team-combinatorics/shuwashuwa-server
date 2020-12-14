58a263bf8c654f6d30cec3789c97b9ecb2189857
leesou <1181902665@qq.com>
2020-12-14 22:11:24 +0800

    测试添加：初步实现了对与微信交互的工具类的测试

---
+++ src/test/java/team/combinatorics/shuwashuwa/utils/WechatUtilTest.java
@@ -0,0 +1,41 @@
+package team.combinatorics.shuwashuwa.utils;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.mysql.cj.xdevapi.JsonArray;
+import com.mysql.cj.xdevapi.JsonString;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import team.combinatorics.shuwashuwa.MainApplication;
+import team.combinatorics.shuwashuwa.model.vo.WechatNoticeVO;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = MainApplication.class)
+public class WechatUtilTest {
+
+    @Test
+    public void testSendNotice() throws Exception {
+        String rescode = "001YRCkl2dzq964kmBml2QsgiD4YRCki";
+        JsonNode root = WechatUtil.getWechatInfo(rescode);
+        String openID = root.path("openid").asText();
+
+        Map<String, String> data = new HashMap<>();
+        data.put("thing4", "45甲331");
+        data.put("date2", "114年514日8:10");
+        data.put("date8", "114年514日19:19");
+        data.put("thing11", "tsugu!");
+        WechatNoticeVO wechatNoticeVO = WechatNoticeVO.builder()
+                .touser(openID)
+                .data(data)
+                .build();
+
+        WechatUtil.sendActivityNotice(wechatNoticeVO);
+    }
+}
