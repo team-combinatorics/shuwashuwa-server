33684a1054d9112f40f0d602182537e55954fbfb
kinami <sunhaonan331@outlook.com>
2020-12-14 21:11:11 +0800

    DAO方法更新：在event表中添加了一个valid_form_id表项，并且提供了修改接口，从而完成了查询摘要列表的功能

--- src/main/java/team/combinatorics/shuwashuwa/model/po/ServiceEventPO.java
+++ src/main/java/team/combinatorics/shuwashuwa/model/po/ServiceEventPO.java
@@ -22,6 +22,8 @@ public class ServiceEventPO {
     private Integer activityId;
     private Integer timeSlot;
     private String problemSummary;
+    // TODO @leo_h 加了一个属性，该属性是非草稿的最后一个维修单的id，默认为null
+    private Integer validFormId;
     private Integer status;
     private Boolean draft;
     private Boolean closed;
