f8488f956799b8b16bf081a8afdb8f7820cf8628
Leo_h <libaolinsy@163.com>
2020-12-17 01:17:20 +0800

    扩充了添加测试用户接口的功能

--- src/main/java/team/combinatorics/shuwashuwa/controller/TestController.java
+++ src/main/java/team/combinatorics/shuwashuwa/controller/TestController.java
@@ -2,20 +2,25 @@ package team.combinatorics.shuwashuwa.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import team.combinatorics.shuwashuwa.annotation.AdminAccess;
 import team.combinatorics.shuwashuwa.annotation.AllAccess;
 import team.combinatorics.shuwashuwa.annotation.NoToken;
 import team.combinatorics.shuwashuwa.annotation.VolunteerAccess;
 import team.combinatorics.shuwashuwa.dao.UserDao;
-import team.combinatorics.shuwashuwa.service.ImageStorageService;
+import team.combinatorics.shuwashuwa.model.dto.AdminDTO;
+import team.combinatorics.shuwashuwa.model.po.UserPO;
+import team.combinatorics.shuwashuwa.service.SuperAdministratorService;
+import team.combinatorics.shuwashuwa.utils.TokenUtil;
 
 @Api(value = "测试用接口",hidden = true)
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/test")
 public class TestController {
     @Value("${spring.datasource.dbcp2.url}")
@@ -27,11 +32,9 @@ public class TestController {
     @Value("${spring.datasource.dbcp2.password}")
     String mysqlPassword;
 
-    @Autowired
-    UserDao userDao;
+    private final UserDao userDao;
 
-    @Autowired
-    ImageStorageService imageStorageService;
+    private final SuperAdministratorService suService;
 
     @ApiOperation("获取当前数据库地址、用户名、密码，用于查看环境变量是否注入成功")
     @GetMapping("/env")
@@ -69,14 +72,34 @@ public class TestController {
         return "Welcome, admin!";
     }
 
-    @ApiOperation("添加4个假身份，测试用")
-    @GetMapping("/addUser")
+    @ApiOperation("添加假用户，返回token")
+    @GetMapping("/fakeUser")
     @NoToken
-    public String addFakeUsers() {
-        userDao.insertUserByOpenid("fake openid 1");
-        userDao.insertUserByOpenid("fake openid 2");
-        userDao.insertUserByOpenid("fake openid 3");
-        userDao.insertUserByOpenid("fake openid 4");
-        return "dokidoki";
+    public String addFakeUsers(
+            @RequestParam("openid") String openid,
+            @RequestParam("volunteer") Boolean volunteer,
+            @RequestParam("admin") Boolean admin
+    ) {
+        userDao.insertUserByOpenid(openid);
+        final UserPO user = userDao.getUserByOpenid(openid);
+        int userid = user.getId();
+        if(volunteer) {
+            userDao.updateUserVolunteerAuthority(userid,true);
+        }
+        if(admin) {
+            suService.addAdministrator(
+                    AdminDTO.builder()
+                            .userid(userid)
+                            .userName("FAKE")
+                            .phoneNumber("1111-1111")
+                            .email("fake@shuwa.shuwa")
+                            .identity("fake")
+                            .studentId("0")
+                            .department("nowhere")
+                    .build()
+            );
+        }
+
+        return TokenUtil.createToken(userid);
     }
 }
