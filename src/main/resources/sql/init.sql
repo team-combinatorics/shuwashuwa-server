d4d2a98ad816f492d159259fde15da2706508ce5
kinami <sunhaonan331@outlook.com>
2020-12-17 01:19:33 +0800

    数据库更新：将createtime和updatetime的精度提升到了微妙级别

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -6,8 +6,8 @@ DROP TABLE IF EXISTS `activity_info`;
 CREATE TABLE `activity_info`
 (
     `id`            INT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT '自增长，活动id',
-    `create_time`   DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `updated_time`  DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `create_time`   DATETIME(6)                 NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间',
+    `updated_time`  DATETIME(6)                 NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '更新时间',
     `start_time`    DATETIME                    NOT NULL COMMENT '预计开始时间',
     `end_time`      DATETIME                    NOT NULL COMMENT '预计开始时间',
     `activity_name` VARCHAR(100)                NOT NULL COMMENT '活动名称',
@@ -21,8 +21,8 @@ DROP TABLE IF EXISTS `activity_time_slot`;
 CREATE TABLE `activity_time_slot`
 (
     `id`           INT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT '自增长，活动id',
-    `create_time`  DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `updated_time` DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `create_time`  DATETIME(6)                 NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间',
+    `updated_time` DATETIME(6)                 NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '更新时间',
     `activity_id`  INT UNSIGNED                NOT NULL COMMENT '活动id',
     `time_slot`    INT UNSIGNED                NOT NULL COMMENT '活动时间段',
     `start_time`   DATETIME                    NOT NULL COMMENT '时间段开始',
@@ -37,8 +37,8 @@ DROP TABLE IF EXISTS `admin`;
 CREATE TABLE `admin`
 (
     `id`           INT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT '自增长',
-    `create_time`  DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `updated_time` DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `create_time`  DATETIME(6)                 NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间',
+    `updated_time` DATETIME(6)                 NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '更新时间',
     `userid`       INT UNSIGNED                NOT NULL COMMENT 'user_id',
     `user_name`    VARCHAR(30)                          DEFAULT NULL COMMENT '真实姓名',
     `phone_number` VARCHAR(30)                          DEFAULT NULL COMMENT '用户手机号',
@@ -58,8 +58,8 @@ DROP TABLE IF EXISTS `cache_pic`;
 CREATE TABLE `cache_pic`
 (
     `id`           INT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT '自增长的表单id',
-    `create_time`  DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `updated_time` DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `create_time`  DATETIME(6)                 NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间',
+    `updated_time` DATETIME(6)                 NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '更新时间',
     `user_id`      INT UNSIGNED                NOT NULL COMMENT '上传用户的id',
     `pic_location` VARCHAR(100)                NOT NULL COMMENT '图片路径，注意手动改一下索引长度',
     INDEX idx_user_id (user_id),
@@ -73,8 +73,8 @@ DROP TABLE IF EXISTS `service_event`;
 CREATE TABLE `service_event`
 (
     `id`               INT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT '自增长的表单id',
-    `create_time`      DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `updated_time`     DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `create_time`      DATETIME(6)                 NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间',
+    `updated_time`     DATETIME(6)                 NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '更新时间',
     `user_id`          INT UNSIGNED                NOT NULL COMMENT '发起维修请求的用户id',
     `volunteer_id`     INT UNSIGNED                         DEFAULT NULL COMMENT '负责维修的志愿者',
     `repairing_result` VARCHAR(100)                         DEFAULT NULL COMMENT '志愿者填写的维修结果',
@@ -101,8 +101,8 @@ DROP TABLE IF EXISTS `service_form`;
 CREATE TABLE `service_form`
 (
     `id`                    INT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT '自增长的表单id',
-    `create_time`           DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `updated_time`          DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `create_time`           DATETIME(6)                 NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间',
+    `updated_time`          DATETIME(6)                 NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '更新时间',
     `brand`                 VARCHAR(15)                          DEFAULT NULL COMMENT '电脑品牌',
     `computer_model`        VARCHAR(30)                          DEFAULT NULL COMMENT '电脑型号',
     `cpu_model`             VARCHAR(30)                          DEFAULT NULL COMMENT 'cpu型号',
@@ -128,8 +128,8 @@ DROP TABLE IF EXISTS `service_pic`;
 CREATE TABLE `service_pic`
 (
     `id`              INT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT '自增长的表单id',
-    `create_time`     DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `updated_time`    DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `create_time`     DATETIME(6)                 NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间',
+    `updated_time`    DATETIME(6)                 NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '更新时间',
     `service_form_id` INT UNSIGNED                         DEFAULT NULL COMMENT '关联的维修单id',
     `pic_location`    VARCHAR(100)                         DEFAULT NULL COMMENT '图片路径',
     INDEX idx_service_form_id (service_form_id),
@@ -142,8 +142,8 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`
 (
     `id`           INT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT '自增长',
-    `create_time`  DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `updated_time` DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `create_time`  DATETIME(6)                 NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间',
+    `updated_time` DATETIME(6)                 NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '更新时间',
     `openid`       VARCHAR(40)                 NOT NULL COMMENT '微信提供的用户id',
     `user_name`    VARCHAR(30)                          DEFAULT NULL COMMENT '该如何称呼您？',
     `phone_number` VARCHAR(30)                          DEFAULT NULL COMMENT '用户手机号',
@@ -172,8 +172,8 @@ DROP TABLE IF EXISTS `volunteer`;
 CREATE TABLE `volunteer`
 (
     `id`           INT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT '自增长',
-    `create_time`  DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `updated_time` DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `create_time`  DATETIME(6)                 NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间',
+    `updated_time` DATETIME(6)                 NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '更新时间',
     `userid`       INT UNSIGNED                NOT NULL COMMENT 'user_id',
     `user_name`    VARCHAR(30)                          DEFAULT NULL COMMENT '真实姓名',
     `phone_number` VARCHAR(30)                          DEFAULT NULL COMMENT '用户手机号',
@@ -194,8 +194,8 @@ DROP TABLE IF EXISTS `volunteer_application`;
 CREATE TABLE `volunteer_application`
 (
     `id`                     INT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT '自增长id',
-    `create_time`            DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `updated_time`           DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `create_time`            DATETIME(6)                 NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间',
+    `updated_time`           DATETIME(6)                 NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '更新时间',
     `user_id`                INT UNSIGNED                NOT NULL COMMENT '申请用户的id',
     `reason_for_application` VARCHAR(100)                NOT NULL COMMENT '申请理由',
     `reply_by_admin`         VARCHAR(100)                         DEFAULT NULL COMMENT '管理员回复',
