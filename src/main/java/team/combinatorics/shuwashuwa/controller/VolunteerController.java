529d5a7fc3bee88ec1830ef611b4d45e9111d4df
kinami <sunhaonan331@outlook.com>
2020-12-17 17:34:31 +0800

    添加了volunteer申请相关的controller和service

---
+++ src/main/java/team/combinatorics/shuwashuwa/controller/VolunteerController.java
@@ -0,0 +1,15 @@
+package team.combinatorics.shuwashuwa.controller;
+
+import io.swagger.annotations.Api;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "志愿者申请相关接口说明")
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/volunteer")
+public class VolunteerController {
+
+
+}
