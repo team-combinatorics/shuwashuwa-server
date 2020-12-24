7853d626f6c45db84e6d457287abd26380ac5171
leesou <1181902665@qq.com>
2020-12-24 22:51:28 +0800

    添加了用于缓存access token的类

---
+++ src/main/java/team/combinatorics/shuwashuwa/utils/AccessToken.java
@@ -0,0 +1,10 @@
+package team.combinatorics.shuwashuwa.utils;
+
+public class AccessToken {
+    
+    /**
+     *
+     * 微信小程序后端的 access token
+     */
+    public static String WX_ACCESS_TOKEN;
+}
