531ade7225a34b0772ebb1abc185703c4af41b81
leesou <1181902665@qq.com>
2020-12-24 22:55:39 +0800

    解决了报错的问题

--- src/main/java/team/combinatorics/shuwashuwa/service/impl/EventServiceImpl.java
+++ src/main/java/team/combinatorics/shuwashuwa/service/impl/EventServiceImpl.java
@@ -111,7 +111,7 @@ public class EventServiceImpl implements EventService {
 
     @Transactional
     @Override
-    public void auditForm(int userid, ServiceEventAuditDTO auditDTO) {
+    public void auditForm(int userid, ServiceEventAuditDTO auditDTO) throws Exception {
         //参数检查和提取
         if (DTOUtil.fieldExistNull(auditDTO))
             throw new KnownException(ErrorInfoEnum.PARAMETER_LACKING);
@@ -144,8 +144,7 @@ public class EventServiceImpl implements EventService {
                 .touser(userDao.getUserByUserid(userid).getOpenid())
                 .data(data)
                 .build();
-        //todo: 这里因为次数限制会导致接口无法使用
-//        WechatUtil.sendAuditResult(wechatNoticeDTO);
+        WechatUtil.sendAuditResult(wechatNoticeDTO);
     }
 
     @Transactional
