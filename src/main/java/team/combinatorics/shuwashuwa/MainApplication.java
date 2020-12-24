587efc23af0ced4cd8110aea7614f0768d6c2275
leesou <1181902665@qq.com>
2020-12-24 22:53:53 +0800

    为主程序添加了开启定时器的注解

--- src/main/java/team/combinatorics/shuwashuwa/MainApplication.java
+++ src/main/java/team/combinatorics/shuwashuwa/MainApplication.java
@@ -3,9 +3,11 @@ package team.combinatorics.shuwashuwa;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
+@EnableScheduling
 @EnableTransactionManagement
 @SpringBootApplication
 @MapperScan(value = "team.combinatorics.shuwashuwa.dao")
